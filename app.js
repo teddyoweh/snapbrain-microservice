4d5d38def6a0d3dbc9b2cbe0d79198ccd90cc5e2
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-11-16 03:37:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -540,10 +540,7 @@ app.listen(PORT, () => {
     const http = require('http');
     const url = require('url');
     
-    const server = http.createServer((req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('WebSocket server\n');
-    });
+    const server = http.createServer(app);
     
     const wss = new WebSocket.Server({ server });
     
